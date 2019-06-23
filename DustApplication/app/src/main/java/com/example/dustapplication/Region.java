1ff8f802378c098ba582a8c500c7ffef9f5e534b
kyu9341 <kyu9341@naver.com>
2019-06-23 21:49:24 +0900

    rere

---
+++ DustApplication/app/src/main/java/com/example/dustapplication/Region.java
@@ -0,0 +1,17 @@
+package com.example.dustapplication;
+
+public class Region {
+    String RegionName;
+
+    public Region(String regionName) {
+        this.RegionName = regionName;
+    }
+
+    public String getRegionName() {
+        return RegionName;
+    }
+
+    public void setRegionName(String regionName) {
+        RegionName = regionName;
+    }
+}
